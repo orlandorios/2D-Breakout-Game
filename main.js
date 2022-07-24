56b9c5184eb1350939392af245a7acac43421cba
Orlando <carlos.orlando.rios@gmail.com>
2022-07-24 17:40:26 -0500

    added game over

--- main.js
+++ main.js
@@ -41,8 +41,17 @@ function draw() {
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
     }    
     
     x += dx;
@@ -83,4 +92,4 @@ function keyUpHandler(e) {
 }
 
 
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
